d1f7ab0fed9736f7394e3d14d3ff2d245daa7497
RitaKruglova <kruglova404@mail.ru>
2024-03-16 19:08:47 +0300

    feat: add styles for header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,9 +6,10 @@ const Home: FC = () => {
     <div className={pageStyles.page} >
       <header className={pageStyles.header} >
         <img className={pageStyles.a} src="/header-image-1.svg" alt="вектор"/>
-        {/* <img className={pageStyles.b} src="/header-image-2.svg" alt="вектор"/>
-        <img className={pageStyles.c} src="/header-image-3.svg" alt="вектор"/> */}
+        <img className={pageStyles.b} src="/header-image-2.svg" alt="вектор"/>
+        <img className={pageStyles.c} src="/header-image-3.svg" alt="вектор"/>
       </header>
+      
     </div>
   )
 }
