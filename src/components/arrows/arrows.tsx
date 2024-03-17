84fa9efcd75ece45403c5567bb8d1f12d2f4d18a
RitaKruglova <kruglova404@mail.ru>
2024-03-17 20:00:32 +0300

    feat: add arrows component

---
+++ src/components/arrows/arrows.tsx
@@ -0,0 +1,12 @@
+import { FC } from 'react';
+import arrowsStyles from './arrows.module.css';
+
+const Arrows: FC = () => {
+  return (
+    <ul className={arrowsStyles.container} >
+      
+    </ul>
+  )
+}
+
+export default Arrows;
