bcd43f8a24d5787c405b934b3e877001d2b68ae6
RitaKruglova <kruglova404@mail.ru>
2024-03-17 19:48:26 +0300

    feat: add styles for medicine

--- src/components/medicine/medicine.tsx
+++ src/components/medicine/medicine.tsx
@@ -3,15 +3,21 @@ import medicineStyles from './medicine.module.css';
 
 interface IMedicineProps {
   iconPath: string;
-  title: string;
+  beforeBold: string;
+  boldPart: string;
+  afterBold: string;
   text: string;
 }
 
-const Medicine: FC<IMedicineProps> = ({ iconPath, title, text }) => {
+const Medicine: FC<IMedicineProps> = ({ iconPath, beforeBold, boldPart, afterBold, text }) => {
   return (
     <li className={medicineStyles.container} >
       <img className={medicineStyles.image} src={iconPath} alt="Иконка" />
-      <h6 className={medicineStyles.title} >{title}</h6>
+      <h6 className={medicineStyles.title} >
+        {beforeBold}
+          <span className={medicineStyles.bold} >{boldPart}</span>
+        {afterBold}
+      </h6>
       <p className={medicineStyles.text} >{text}</p>
     </li>
   )
