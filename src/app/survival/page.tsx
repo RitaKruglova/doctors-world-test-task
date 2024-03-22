b4126b0b50e2246825ae3c18c841ab59d4816f09
RitaKruglova <kruglova404@mail.ru>
2024-03-22 19:52:39 +0300

    feat: add survival page

---
+++ src/app/survival/page.tsx
@@ -0,0 +1,17 @@
+import { FC } from 'react';
+import survivalStyles from './page.module.css';
+
+const Survival: FC = () => {
+  return (
+    <div className={survivalStyles.container} >
+      <h4 className={survivalStyles.subtitle} >Большинство пациентов с ХОБЛ столкнется с обострениями: как минимум одно среднетяжелое или тяжелое обострение случится в течение 3 лет у 77% пациентов1.</h4>
+      <p className={survivalStyles.text} >В течение 5 лет после первого тяжёлого обострения в живых остаются только 40% пациентов.2 Прогноз после обострений ХОБЛ схож с таковым при сердечной недостаточности, инфаркте миокарда и некоторых злокачественных опухолях.</p>
+      <h3 className={survivalStyles.title} >Пятилетняя выживаемость пациентов</h3>
+      <div className={survivalStyles.cards} >
+        
+      </div>
+    </div>
+  )
+}
+
+export default Survival;
